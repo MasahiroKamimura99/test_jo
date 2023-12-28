63ed6787671d3e6579e0db2c622b73425d0828bd
MasahiroKamimura99 <59987173+MasahiroKamimura99@users.noreply.github.com>
2023-12-28 10:37:38 +0900

    Update jojo.py

--- jojo.py
+++ jojo.py
@@ -57,7 +57,6 @@ def main():
         df1 = pd.read_csv('frequency.csv', encoding='utf_8_sig')
         df_out1 = pd.concat([df1, df_tmp], axis=0)
         df_out1.to_csv('frequency.csv', encoding='utf_8_sig', index=False)
-        st.dataframe(df_out1)
 
         my_bar = st.progress(0)
         for percent_complete in range(100):
@@ -88,8 +87,6 @@ def main():
         st.markdown("# "+st.session_state.key_1)
         st.markdown("## "+st.session_state.key_2)
         st.image(st.session_state.key_3)
-        
-
 
     if st.session_state.count>10:
         st.markdown(str(st.session_state.count)+"回実行しています"
@@ -101,6 +98,9 @@ def main():
         st.sidebar.markdown("""
                     ---
                     """)
+        st.text('アクセス回数')
+        st.sidebar.dataframe(df_out1)
+
         # with st.sidebar.expander("もしよかったら「いいね」、もしくは「感想・ご意見」をください。"):        
         if st.sidebar.button("イイねッ！"):
             dt_now = datetime.datetime.now()
